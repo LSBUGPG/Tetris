85a5d17704429678396876743f8d75cafdfd6a3f
Paul Sinnett <paul.sinnett@gmail.com>
2020-04-16 14:01:20 +0100

    update from Lukas

--- Assets/Scripts/Gameplay/TetrisBlock.cs
+++ Assets/Scripts/Gameplay/TetrisBlock.cs
@@ -14,12 +14,9 @@ public class TetrisBlock : MonoBehaviour
     public float[] hackTimer;
     public bool hacked;
     public bool coolingHack;
-    private static Transform[, ] grid = new Transform[width, height];
+    private static Transform[,] grid = new Transform[width, height];
 
-    public Button leftButton;
-    public Button rightButton;
-    public Button rotateButton;
-    public Button downButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,135 +27,155 @@ public class TetrisBlock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+         if (Input.GetKeyDown(KeyCode.LeftArrow))
+         {
+            MoveLeft();
+         }
+         else if (Input.GetKeyDown(KeyCode.RightArrow))
+         {
+            MoveRight();
+         }
+         else if (Input.GetKeyDown(KeyCode.UpArrow))
+         {
+            Rotate();
+         }
+
+         if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+         {
+            MoveDown();
+         }
+    }
+
+    public void MoveLeft()
+    {
+        transform.position += new Vector3(-1, 0, 0);
+        if (!ValidMove())
+            transform.position -= new Vector3(-1, 0, 0);
+    }
+
+    public void MoveRight()
+    {
+        transform.position += new Vector3(1, 0, 0);
+        if (!ValidMove())
+            transform.position -= new Vector3(1, 0, 0);
+    }
+
+    public void Rotate()
+    {
+        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+        if (!ValidMove())
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+    }
+
+    public void MoveDown()
+    {
+        transform.position += new Vector3(0, -1, 0);
+        if (!ValidMove())
         {
-            transform.position += new Vector3(-1, 0, 0);
-            if (!ValidMove())
-                transform.position -= new Vector3(-1, 0, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(1, 0, 0);
-            if (!ValidMove())
-                transform.position -= new Vector3(1, 0, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-            if (!ValidMove())
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+            transform.position -= new Vector3(0, -1, 0);
+            AddToGrid();
+            CheckForLines();
+
+            this.enabled = false;
+            FindObjectOfType<SpawnTetrimino>().NewTetrimino();
         }
 
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        // Hacker();
+
+        previousTime = Time.time;
+    }
+
+    void CheckForLines()
+    {
+        for (int i = height - 1; i >= 0; i--)
         {
-            transform.position += new Vector3(0, -1, 0);
-            if (!ValidMove())
+            if (HasLine(i))
             {
-                transform.position -= new Vector3(0, -1, 0);
-                AddToGrid();
-                CheckForLines();
-
-                this.enabled = false;
-                FindObjectOfType<SpawnTetrimino>().NewTetrimino();
+                DeleteLine(i);
+                RowDown(i);
             }
-
-            // Hacker();
-
-            previousTime = Time.time;
         }
+    }
 
-        void CheckForLines()
+    bool HasLine(int i)
+    {
+        for (int j = 0; j < width; j++)
         {
-            for (int i = height - 1; i >= 0; i--)
-            {
-                if (HasLine(i))
-                {
-                    DeleteLine(i);
-                    RowDown(i);
-                }
-            }
+            if (grid[j, i] == null)
+                return false;
         }
 
-        bool HasLine(int i)
+        return true;
+    }
+
+    void DeleteLine(int i)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Destroy(grid[j, i].gameObject);
+            grid[j, i] = null;
+        }
+    }
+
+    void RowDown(int i)
+    {
+        for (int y = i; y < height; y++)
         {
             for (int j = 0; j < width; j++)
             {
-                if (grid[j, i] == null)
-                    return false;
-            }
-
-            return true;
-        }
-
-        void DeleteLine(int i)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                Destroy(grid[j, i].gameObject);
-                grid[j, i] = null;
-            }
-        }
-
-        void RowDown(int i)
-        {
-            for (int y = i; y < height; y++)
-            {
-                for (int j = 0; j < width; j++)
+                if (grid[j, y] != null)
                 {
-                    if (grid[j, y] != null)
-                    {
-                        grid[j, y - 1] = grid[j, y];
-                        grid[j, y] = null;
-                        grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
-                    }
+                    grid[j, y - 1] = grid[j, y];
+                    grid[j, y] = null;
+                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
                 }
-
             }
-        }
 
-        void AddToGrid()
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
         {
-            foreach (Transform children in transform)
-            {
-                int roundedX = Mathf.RoundToInt(children.transform.position.x);
-                int roundedY = Mathf.RoundToInt(children.transform.position.y);
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
 
-                grid[roundedX, roundedY] = children;
-            }
+            grid[roundedX, roundedY] = children;
         }
+    }
 
-        bool ValidMove()
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
         {
-            foreach (Transform children in transform)
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
             {
-                int roundedX = Mathf.RoundToInt(children.transform.position.x);
-                int roundedY = Mathf.RoundToInt(children.transform.position.y);
-
-                if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
-                {
-                    return false;
-                }
-
-                if (grid[roundedX, roundedY] != null)
-                    return false;
+                return false;
             }
 
-            return true;
+            if (grid[roundedX, roundedY] != null)
+                return false;
         }
+
+        return true;
     }
 
     void Hacker()
     {
-        /* int hackWaitTime = Random.Range(0, hackTimer.Length);
+       /* int hackWaitTime = Random.Range(0, hackTimer.Length);
 
-         if (coolingHack == true)
-         {
-             hackTimer = null;
-         }
+        if (coolingHack == true)
+        {
+            hackTimer = null;
+        }
 
-         if (hacked == true)
-         {
-             fallTime = 0;
-         } */
-    }
+        if (hacked == true)
+        {
+            fallTime = 0;
+        } */
+    }    
 }
