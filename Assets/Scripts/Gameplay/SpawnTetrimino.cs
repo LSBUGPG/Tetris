d3506977ae72257f7db4511b2a22949a13a86724
Paul Sinnett <paul.sinnett@gmail.com>
2020-04-16 13:16:51 +0100

    Initial commit

---
+++ Assets/Scripts/Gameplay/SpawnTetrimino.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTetrimino : MonoBehaviour
+{
+    public GameObject[] Tetriminoes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetrimino();
+    }
+
+    // Update is called once per frame
+    public void NewTetrimino()
+    {
+        Instantiate(Tetriminoes[Random.Range(0, Tetriminoes.Length)], transform.position, Quaternion.identity);
+    }
+}
